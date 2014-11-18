500a2267f64b6a86758f8f5f48763dead74f0431
thundervoice <dmbbandola@gmail.com>
2014-11-18 21:00:50 +0800

    updated withdraw and if_int

--- account.py
+++ account.py
@@ -4,11 +4,11 @@ class Account(object):
         self.account_number = account_number
         self.balance = balance
 
-    def check_if_int(self, balance):
-        if type(balance) == int:
+    def check_if_int(self,account_number, balance):
+        if type(account_number) != int and type(balance) == int:
             return self.balance
         else:
-            raise TypeError("Invalid type: {}".format(type(balance)))
+            raise TypeError("Invalid type: {} and {}".format(type(account_number),type(balance)))
 
 
 if __name__ == '__main':
--- bank.py
+++ bank.py
@@ -7,3 +7,16 @@ class Bank(object):
 
     def get_account_balance(self, account_number):
         return self.accounts.get(account_number)
+
+    def check_account(self, account):
+        if account == account.account_number:
+            return self.accounts[account.account_number]
+        else:
+            print "Account nonexistent"
+
+    def withdraw(self, account_number, balance,  amount):
+        if balance > amount:
+            withdrawal = balance - amount
+            return withdrawal
+        else:
+            print "Insufficient Amount"
